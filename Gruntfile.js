6bd5be0f88370f81c9fd63565e103b32e0a89326
戊子 <xianqian.rxq@alibaba-inc.com>
2014-07-15 21:05:05 +0800

    demo 10%

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,18 +5,35 @@
  */
 
 module.exports = function (grunt) {
-	'use strict';
+    'use strict';
 
-	// Project configuration.
-	grunt.initConfig({
+    // Project configuration.
+    grunt.initConfig({
+        mtc: {
+            options: {
 
-	});
+            },
+            build: {
+                src: '',
+                dest: ''
+            }
+        },
+        watch: {
 
-	// These plugins provide necessary tasks.
-	grunt.loadNpmTasks('grunt-contrib-qunit');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+        }
+    });
 
-	// Default task.
-	grunt.registerTask('default', ['jshint']);
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task.
+    grunt.registerTask('default', ['jshint']);
+
+    // Demo task.
+    grunt.registerTask('demo', ['jshint']);
 };
